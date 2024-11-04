d2abcfe6015d232155aa3bf50cebcb60945d18ee
EmmaEmmEmm <103096574+EmmaEmmEmm@users.noreply.github.com>
2024-11-04 18:03:43 +0000

    a

--- a.py
+++ a.py
@@ -1,2 +1,16 @@
-a = input()
-print(a)
+lineOne = input().split()
+lineTwo = input().split()
+
+num = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+color = ['S', 'H', 'D', 'C']
+test = []
+for i in range(0, len(num)):
+    for j in range(0, len(color)):
+        test.append(num[i] + color[j])
+
+def error(a):
+    for i in a:
+        if i not in test:
+            return 1
+print(error(lineOne))
+print(error(lineTwo))
