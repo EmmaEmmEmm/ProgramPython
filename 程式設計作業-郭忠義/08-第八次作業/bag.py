cbf12100b7f1417b5f299d37b39991aa12648da7
EmmaEmmEmm <103096574+EmmaEmmEmm@users.noreply.github.com>
2024-11-27 13:03:25 +0000

    sc

---
+++ 程式設計作業-郭忠義/08-第八次作業/bag.py
@@ -0,0 +1,9 @@
+a = input().split()
+b = input()
+
+def f(n):
+    
+if b > 10 or b < 1:
+    print("Input Error")
+elif len(a) != len(set(a)):
+    print("Duplicated ID")
--- 程式設計作業-郭忠義/08-第八次作業/number.py
+++ 程式設計作業-郭忠義/08-第八次作業/number.py
@@ -2,3 +2,11 @@ list1 = []
 for _ in range(4):
     a = input().split()
     list1.append(a)
+w, x, y, z = 0
+for i in range(2):
+    for j in range(2):
+        if list[i][j] == 0:w+=1
+        if list[i][j+2] == 0:x+=1
+        if list[i+2][j] == 0:y+=1
+        if list [i+2][j+2] == 0:z+=1
+
